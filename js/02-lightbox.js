4877666499b5c3a1f70d9c588ef873c44293f8a1
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2021-10-30 14:08:02 +0300

    task 2
    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = renderGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function renderGallery(items) {
+  return items
+    .map(
+      ({ original, preview, description }) =>
+        `<li>
+          <a class="gallery__item" href="${original}">
+              <img
+                  class="gallery__image"
+                  src="${preview}"
+                  alt="${description}"
+              />
+          </a>
+        </li>`,
+    )
+    .join(' ');
+}
+
+const modalLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
